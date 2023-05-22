79434626c937770704d2a024714cd6330fccc108
jacrue <ruesch0311@gmail.com>
2023-05-22 18:01:20 -0500

    Team 2 activity

--- src/js/product.js
+++ src/js/product.js
@@ -16,3 +16,11 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+// added by team 5 , import Param.
+import { findProductById } from "./productData.mjs";
+import { setLocalStorage, getParam } from "./utils.mjs";
+
+const productId = getParam("product");
+
+console.log(findProductById(productId));
