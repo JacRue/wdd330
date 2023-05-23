750a4ccfd079eef363c7ee46a21f995b4d7bb6d8
jacrue <ruesch0311@gmail.com>
2023-05-22 19:21:03 -0500

    Fixed issues with team2 activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,26 +1,6 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
-
-function addProductToCart(product) {
-  const cart = getLocalStorage("so-cart") || [];
-  const updatedCart = [...cart, product];
-  setLocalStorage("so-cart", updatedCart); 
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-// added by team 5 , import Param.
-import { findProductById } from "./productData.mjs";
-import { setLocalStorage, getParam } from "./utils.mjs";
+//Team had issues on team2 assignment starting on step 6, see Ponder: Team Activity 2 paper for details /
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
 const productId = getParam("product");
-
-console.log(findProductById(productId));
+productDetails(productId);
